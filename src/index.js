451dec781069d50dfa429effdf404392f5a77409
Maksym Bezvuhliak <maximbezvugliak@gmail.com>
2022-11-26 21:45:39 +0200

    workflow to publish package

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 export {createStore} from './store/createStore'
 export {connect} from './store/connect'
 export {combineReducers} from './store/combineReducers'
-
-export default {
-    createStore,
-    connect,
-    combineReducers
-}
